cb9856c04b3f001330f8163e8cecbdc61a13e934
Андрей Сошин <a_soshin@mail.ru>
2020-06-17 22:39:17 +0300

    123

--- ClientApp/src/App.tsx
+++ ClientApp/src/App.tsx
@@ -3,28 +3,42 @@ import { Container, Navbar, DropdownToggle, DropdownMenu, DropdownItem, Dropdown
 
 import './custom.css'
 
-export default class App extends React.Component<{}> {
-
+export default class App extends React.PureComponent<{ children?: React.ReactNode }> {
+  //constructor(props: { children?: React.ReactNode }) {
+  //  super(props)
+  //}
   render() {
-    return <header>
-      <Navbar className="navbar-expand navbar-toggleable border-bottom box-shadow mb-3" light>
-        <Container className="justify-content-start">
-          <Button className="nbutton mr-1" id="prev_page" color="primary" href="#">&lt;&lt;</Button>
-          <Button className="nbutton mr-1" id="prev_period" color="primary">&lt;</Button>
-          <Button className="nbutton mr-1" id="next_period" color="primary" href="#">&gt;</Button>
-          <Button className="nbutton" id="next_page" color="primary" href="#">&gt;&gt;</Button>
-          <UncontrolledDropdown>
-            <DropdownToggle nav caret>
-              Линия времени
+    return (
+      <React.Fragment>
+        <header>
+          <Navbar className="navbar-expand navbar-toggleable border-bottom box-shadow mb-3" light>
+            <Container className="justify-content-start mx-0">
+              <Button className="nbutton mr-1" id="prev_page" outline href="#">&lt;&lt;</Button>
+              <Button className="nbutton mr-1" id="prev_period" outline>&lt;</Button>
+              <Button className="nbutton mr-1" id="next_period" outline href="#">&gt;</Button>
+              <Button className="nbutton" id="next_page" outline href="#">&gt;&gt;</Button>
+              <UncontrolledDropdown>
+                <DropdownToggle nav caret>
+                  Линия времени
                 </DropdownToggle>
-            <DropdownMenu>
-              <DropdownItem>Новая</DropdownItem>
-              <DropdownItem>Загрузить</DropdownItem>
-              <DropdownItem>Загрузить файл</DropdownItem>
-            </DropdownMenu>
-          </UncontrolledDropdown>
+                <DropdownMenu>
+                  <DropdownItem>Новая</DropdownItem>
+                  <DropdownItem>Загрузить</DropdownItem>
+                  <DropdownItem>Загрузить файл</DropdownItem>
+                </DropdownMenu>
+              </UncontrolledDropdown>
+            </Container>
+            <Container fluid className="justify-content-end">
+              <div className="mr-2">Иванов</div>
+              <Button color="primary" className="mr-1" outline onClick={() => alert('1')}>Регистрация</Button>
+              <Button color="primary" outline onClick={() => alert('2')}>Вход</Button>
+            </Container>
+          </Navbar>
+        </header>
+        <Container className="mx-0">
+          {this.props.children}
         </Container>
-      </Navbar>
-    </header>
+      </React.Fragment>
+    )
   }
 }
