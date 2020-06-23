e3c534fd5447d4459ec565083bd0cc992e94c235
Андрей Сошин <a_soshin@mail.ru>
2020-06-23 15:53:02 +0300

    444

--- ClientApp/src/components/TLPeriod.tsx
+++ ClientApp/src/components/TLPeriod.tsx
@@ -2,10 +2,10 @@
 import { Container, Navbar, DropdownToggle, DropdownMenu, DropdownItem, Button, UncontrolledDropdown } from 'reactstrap';
 import './tlperiod.css'
 
-const TLPeriod = (props) => {
-  return (
-    <h1></h1>
-    )
+export class TLPeriod extends React.PureComponent<{}, {}>{
+  render() {
+    return (
+      <td id={this.props.Id}></td>
+      )
+      }
 }
-
-export default TLPeriod;
