5f1af403fd6f400c995222da331643e1bc30b8fd
Андрей Сошин <a_soshin@mail.ru>
2020-06-23 23:57:14 +0300

    322

--- ClientApp/src/components/TLPeriod.js
+++ ClientApp/src/components/TLPeriod.js
@@ -1,4 +1,29 @@
 "use strict";
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
 Object.defineProperty(exports, "__esModule", { value: true });
+var React = require("react");
 require("./tlperiod.css");
+var TPeriod = /** @class */ (function (_super) {
+    __extends(TPeriod, _super);
+    function TPeriod() {
+        return _super !== null && _super.apply(this, arguments) || this;
+    }
+    TPeriod.prototype.react = function () {
+        return (React.createElement("td", { draggable: true, className: "period_cell", id: 'cell-' + this.props.indexOfTL + '-' + this.props.id }));
+    };
+    return TPeriod;
+}(React.PureComponent));
+exports.TPeriod = TPeriod;
 //# sourceMappingURL=TLPeriod.js.map
